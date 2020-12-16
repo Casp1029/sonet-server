d29b4303e19a840fe0f5af7213c156710ba06d3e
Casper <cadlh@hotmail.com>
2020-12-16 01:45:32 +0100

    added config

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB: 
+    'mongodb+srv://classed:a5Wqiyg4Tga71CV7@cluster0.k9q7u.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+};
